8d25d87d0ccbaaceae815e46c8e32aa58e6371b4
25717594@qq.com <25717594@qq.com>
2015-04-19 11:36:48 +0800

    add routing

---
+++ 04_Routing/04_Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //exchange类型设为direct，不适用fanout的广播模式
+                    //客户端和服务端都需要设置
+                    channel.ExchangeDeclare("direct_logs", "direct");
+
+                    //var severity = (args.Length > 0) ? args[0] : "info";
+                    //var message = (args.Length > 1)
+                    //                ? string.Join(" ", args.Skip(1).ToArray())
+                    //                : "Hello World!";
+                    string[] argsSeverity = new string[] { "info", "warning", "error" };
+                    string severity = argsSeverity[new Random().Next(0, 3)];
+                    var message = severity + "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("direct_logs", severity, null, body);
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+                }
+            }
+        }
+    }
+}
---
+++ 04_Routing/04_Server/Program.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//http://www.rabbitmq.com/tutorials/tutorial-four-dotnet.html
+//For example we may want the script which is writing log messages to the disk to only receive critical errors, 
+//and not waste disk space on warning or info log messages.
+//服务端绑定特定的RoutingKey，可以一个或多个；客户端绑定特定的RoutingKey
+//客户端发送时只将消息发送到匹配RoutingKey的服务端
+namespace _04_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var r = new Random().Next(0, 3);
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //exchange类型设为direct，不适用fanout的广播模式
+                    //客户端和服务端都需要设置
+                    channel.ExchangeDeclare("direct_logs", "direct");
+
+                    //随机声明队列名称
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    //if (args.Length < 1)
+                    //{
+                    //    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                    //                            Environment.GetCommandLineArgs()[0]);
+                    //    Environment.ExitCode = 1;
+                    //    return;
+                    //}
+
+                    //string[] argsSeverity = new string[] { "info", "warning", "error" };
+                    string[] argsSeverity = new string[] { "info"};
+                    foreach (var severity in argsSeverity)
+                    {
+                        channel.QueueBind(queueName, "direct_logs", severity);
+                    }
+
+                    Console.WriteLine(" [*] Waiting for messages. " +
+                                      "To exit press CTRL+C");
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume(queueName, true, consumer);
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                          routingKey, message);
+                    }
+                }
+            }
+        }
+    }
+}
