7a0c9734c8d15d506508647511758d8fccae6ebc
JamesFoxChen <25717594@qq.com>
2015-05-08 18:03:56 +0800

    add time cal

--- 01_HelloWorld/Client/_01_Client_Program.cs
+++ 01_HelloWorld/Client/_01_Client_Program.cs
@@ -1,6 +1,7 @@
 ﻿using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Client
     class _01_Client_Program
     {
         private static string queueName = "01";
+        private static int count = 100000;
 
         static void Main(string[] args)
         {
@@ -25,8 +27,22 @@ namespace Client
                     string message = "Hello World!" + "_" + DateTime.Now.ToString();
                     var body = Encoding.UTF8.GetBytes(message);
 
-                    channel.BasicPublish("", queueName, null, body);
-                    Console.WriteLine(" [x] Sent {0}", message);
+                    var sw = new Stopwatch();
+                    sw.Start();
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        channel.BasicPublish("", queueName, null, body);
+                    }
+                    sw.Stop();
+
+                    TimeSpan ts = sw.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:时 {1:00}:分 {2:00}:秒：{3:00}:毫秒",
+                                                                   ts.Hours, ts.Minutes, ts.Seconds,
+                                                                   ts.Milliseconds / 10);
+
+                    Console.WriteLine(" Count:{0} Producer Complete.Total Time:{1}", count, elapsedTime);
+                    Console.ReadLine();
                 }
             }
         }
--- 01_HelloWorld/Server/_01_Server_Program.cs
+++ 01_HelloWorld/Server/_01_Server_Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -31,16 +32,36 @@ namespace Server
 
                     Console.WriteLine(" [*] Waiting for messages." +
                                              "To exit press CTRL+C");
+
+                    var sw = new Stopwatch();
+                    sw.Start();
+
                     while (true)
                     {
                         //接受客户端发送的消息并打印出来
-                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                        //var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        BasicDeliverEventArgs ea = null;
+                        consumer.Queue.Dequeue(2000, out ea);
+
+                        if (ea == null)
+                        {
+                            sw.Stop();
+
+                            TimeSpan ts = sw.Elapsed;
+                            string elapsedTime = String.Format("{0:00}:时 {1:00}:分 {2:00}:秒：{3:00}:毫秒",
+                                                                 ts.Hours, ts.Minutes, ts.Seconds,
+                                                                 ts.Milliseconds / 10);
+
+                            Console.WriteLine(" [x] Consumer Complete.Total Time:{0}", elapsedTime);
+                            Console.ReadLine();
+                        }
 
                         var body = ea.Body;
                         var message = Encoding.UTF8.GetString(body);
-                        Console.WriteLine(" [x] Received {0}", message);
+                        //Console.WriteLine(" [x] Received {0}", message);
 
-                        Thread.Sleep(5000);
+                        //Thread.Sleep(5000);
                     }
                 }
             }
